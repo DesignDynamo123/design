9131ccb12816ab23abe2397ddacdea679eb6408c
DesignDynamo123 <dsgndnm@gmail.com>
2024-11-01 11:32:50 +0530

    Update settings.py

--- designdynamo/settings.py
+++ designdynamo/settings.py
@@ -113,8 +113,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    "BASE_DIR / 'design_dynamo/static',
+    os.path.join(BASE_DIR, 'design_dynamo/static'), 
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
